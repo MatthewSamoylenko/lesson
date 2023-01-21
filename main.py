76216f6adce6842abe30882a65cc6ac36ac89ab4
apostol <dekspeen@gmail.com>
2023-01-21 23:08:23 +0200

    Dz-4

--- main.py
+++ main.py
@@ -1,22 +1,66 @@
-dictionary = {"cat": "Котейка"}
-print(dictionary)
-print(dictionary["cat"])
+print("Select level difficulty")
 
-animal = "cat"
-dictionary[animal] = "kot"
+easy = {"be": "быть", "do": "делать", "have": "иметь", "make": "делать", "go": "идти"}
+medium = {"be": "быть", "do": "делать", "have": "иметь", "make": "делать", "go": "идти"}
+hard = {"be": "быть", "do": "делать", "have": "иметь", "make": "делать", "go": "идти"}
 
-print(dictionary[animal])
+levels = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5"
+}
 
-del dictionary["cat"]
+answers = {}
 
-print(dictionary)
+selected_level = input("easy, medium, hard \n")
 
-stupid_list = {"Name": "Alex", "Age": "33", "Address": "Himki"}
-print(stupid_list.keys())
-print(stupid_list.values())
+if selected_level == "easy":
+    for key, item in easy.items():
+        len_words = len(item)
+        print(f"{key},{len_words}, The first sing {item[0]}")
+        answer = input()
+        if answer != item:
+            print(f" Error, {key} it's {item}")
+        answers[key] = (answer == item)
 
-for key in stupid_list:
-    print(key)
+if selected_level == "medium":
+    for key, item in medium.items():
+        len_words = len(item)
+        print(f"{key},{len_words}, The first sing {item[0]}")
+        answer = input()
+        if answer != item:
+            print(f" Error, {key} it's {item}")
+        answers[key] = (answer == item)
 
-for key, item in stupid_list.items():
-    print(key,item)
+if selected_level == "hard":
+    for key, item in hard.items():
+        len_words = len(item)
+        print(f"{key},{len_words}, The first sing {item[0]}")
+        answer = input()
+        if answer != item:
+            print(f" Error, {key} it's {item}")
+        answers[key] = (answer == item)
+
+
+print("True")
+for key, item in answers.items():
+    if item == True:
+        print(key)
+
+print("False")
+for key, item in answers.items():
+    if item == False:
+        print(key)
+
+
+
+count_answer = 0
+for key, item in answers.items():
+    if item == True:
+       count_answer += 1
+
+
+print(f"Your level {levels[count_answer]}")
