e49c29bd5753e0ef5758a188aa87b2ea7b59fab3
apostol <dekspeen@gmail.com>
2023-01-21 21:10:26 +0200

    trein str

---
+++ main.py
@@ -0,0 +1,19 @@
+email = "support@gmail.com"
+print(email.count('@'))
+
+str_test = 'sdfsadf asdf asdf asd asdf a asdf '
+print(str_test.count(' '))
+
+email = "my new mail@gmail.com"
+print(email.replace(' ', '_'))
+print(email.title())
+
+s = "docs.google.com"
+s = s.split(".")
+
+print(s)
+print(type(s))
+
+print(s[2])
+
+print('.'.join(s))
